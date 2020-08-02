5fc60adbe696a137215473ac7d517a9548184337
Ride0401 <devarata.oza@gmail.com>
2020-08-02 19:28:31 +0530

    Basic layout structure

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxTHAj0vfNCTinCCDFjH3LnnrG_MBOq-M",
+  authDomain: "clone-a19de.firebaseapp.com",
+  databaseURL: "https://clone-a19de.firebaseio.com",
+  projectId: "clone-a19de",
+  storageBucket: "clone-a19de.appspot.com",
+  messagingSenderId: "837383925248",
+  appId: "1:837383925248:web:df4539bc5569b49edfb2e5",
+  measurementId: "G-SR6QN3SZY7"
+};
