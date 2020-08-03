8bc9a98290e68bb6a0377f4200930b8fe813b50c
Ride0401 <devarata.oza@gmail.com>
2020-08-03 14:15:58 +0530

    Responsive : Flex-wrap

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import "./RecommendedVideos.css"
 import VideoCard from './VideoCard.js'
+import logo1 from './logos/logo1.jpg'
+import logo2 from './logos/logo2.png'
+import logo3 from './logos/logo3.png'
+import logo4 from './logos/logo4.png'
+import thumbnail1 from './thumbnails/thumbnail1.png'
+import thumbnail2 from './thumbnails/thumbnail2.png'
+import thumbnail3 from './thumbnails/thumbnail3.png'
+import thumbnail4 from './thumbnails/thumbnail4.png'
+
+
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
@@ -10,16 +20,34 @@ function RecommendedVideos() {
           title="Top 5 Website to learning web design & development"
           views="2.3M Views"
           timestamp="3 days ago"
-          channelImage="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi0.wp.com%2Fsellersessions.com%2Fwp-content%2Fuploads%2F2019%2F04%2FICON-Youtube-channel.png%3Ffit%3D481%252C481%26ssl%3D1&imgrefurl=https%3A%2F%2Fsellersessions.com%2Ficon-youtube-channel%2F&tbnid=h7dzpweVWrCoNM&vet=12ahUKEwiC6fvjmf3qAhULFisKHdozDzsQMygGegUIARDeAQ..i&docid=7yggfeoG4XLFIM&w=481&h=481&q=youtube%20channel%20icon&ved=2ahUKEwiC6fvjmf3qAhULFisKHdozDzsQMygGegUIARDeAQ"
-          channelName="React Forever"
-          image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.technotification.com%2Fwp-content%2Fuploads%2F2017%2F06%2FBest-youtube-channels-to-learn-coding-2.jpg&imgrefurl=https%3A%2F%2Fwww.technotification.com%2F2017%2F06%2Flearn-coding-online-youtube-channels.html&tbnid=0DQoP07WcfPGLM&vet=12ahUKEwjvtKS_mf3qAhU9_TgGHV3GAV8QMygPegUIARDKAQ..i&docid=3uUVKc0v3PDbEM&w=1200&h=700&q=youtube%20coding&ved=2ahUKEwjvtKS_mf3qAhU9_TgGHV3GAV8QMygPegUIARDKAQ"
+          channelImage={logo1}
+          channelName="SolidGeek"
+          image={thumbnail1}
         />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
+        <VideoCard
+          title="Learn Angular in 5 hours"
+          views="4.5M views"
+          timestamp="2 months ago"
+          channelImage={logo2}
+          channelName="Force Runner"
+          image={thumbnail2}
+        />
+        <VideoCard
+          title="How to lose weight in 4 weeks"
+          views="435K views"
+          timestamp="1 year ago"
+          channelImage={logo3}
+          channelName="Witness Fitness"
+          image={thumbnail3}
+         />
+        <VideoCard
+          title="Cheating - Stand Up Comedy"
+          views="1.2M views"
+          timestamp="2 weeks ago"
+          channelImage={logo4}
+          channelName="Comedy Nights"
+          image={thumbnail4}
+        />
       </div>
     </div>
   )
